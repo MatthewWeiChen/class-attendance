b214377dca33e0f3756b9041306d2fa266535248
MattWeiChen <mattchen1021@gmail.com>
2025-04-08 17:35:24 -0700

    adding auth

--- src/auth/auth.services.ts
+++ src/auth/auth.services.ts
@@ -4,10 +4,11 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signin() {
-    return { msg: 'I am signed in' };
-  }
+
   signup() {
     return { msg: 'I am signed up' };
+  }
+  signin() {
+    return { msg: 'I am signed in' };
   }
 }
